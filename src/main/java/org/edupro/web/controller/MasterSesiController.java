1412d57bbbb3d02748eae707dbc4c609a3c1fefe
sabilmr <r4089077@gmail.com>
2024-04-27 08:29:30 +0700

    feat: sesi

--- src/main/java/org/edupro/web/controller/MasterSesiController.java
+++ src/main/java/org/edupro/web/controller/MasterSesiController.java
@@ -3,12 +3,17 @@ package org.edupro.web.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.edupro.web.model.request.SesiRequest;
+import org.edupro.web.model.response.KurikulumResponse;
 import org.edupro.web.model.response.Response;
 import org.edupro.web.model.response.SesiResponse;
+import org.edupro.web.model.response.TahunAjaranResponse;
+import org.edupro.web.service.MasterKurikulumService;
 import org.edupro.web.service.MasterSesiService;
+import org.edupro.web.service.TahunAjaranService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -20,6 +25,9 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class MasterSesiController extends BaseController<SesiResponse>{
     private final MasterSesiService service;
+    private final TahunAjaranService tahunAjaranService;
+    private final MasterKurikulumService kurikulumService;
+
     @GetMapping
     public ModelAndView index(){
         var view = new ModelAndView("pages/master/sesi/index");
@@ -29,7 +37,14 @@ public class MasterSesiController extends BaseController<SesiResponse>{
 
     @GetMapping("/add")
     public ModelAndView add(){
-        return new ModelAndView("pages/master/sesi/add");
+        ModelAndView view = new ModelAndView("pages/master/sesi/add");
+        List<TahunAjaranResponse> tahunAjaran = this.tahunAjaranService.get();
+        List<KurikulumResponse> kurikulum = this.kurikulumService.get();
+
+        view.addObject("sesi", new SesiRequest());
+        view.addObject("dataTa", tahunAjaran);
+        view.addObject("kurikulum", kurikulum);
+        return view;
     }
 
     @GetMapping("/data")
@@ -39,30 +54,75 @@ public class MasterSesiController extends BaseController<SesiResponse>{
     }
 
     @PostMapping("/save")
-    public ResponseEntity<Response> save(@RequestBody @Valid SesiRequest request){
-        var result = service.save(request);
-        return getResponse(result);
+    public ModelAndView save(@ModelAttribute("sesi") @Valid SesiRequest request, BindingResult result){
+        ModelAndView view = new ModelAndView("pages/master/sesi/add");
+        List<TahunAjaranResponse> tahunAjaran = this.tahunAjaranService.get();
+        List<KurikulumResponse> kurikulum  = this.kurikulumService.get();
+        if (result.hasErrors()){
+            view.addObject("sesi", request);
+            return view;
+        }
+
+        view.addObject("dataTa", tahunAjaran);
+        view.addObject("kurikulum", kurikulum);
+        var response = service.save(request);
+        return new ModelAndView("redirect:/master/sesi");
     }
 
     @GetMapping("/edit/{id}")
-    public ModelAndView edit(@PathVariable("id") Integer id){
-        return new ModelAndView("pages/master/sesi/edit");
+    public ModelAndView edit(@PathVariable("id") String id){
+        ModelAndView view = new ModelAndView("pages/master/sesi/edit");
+        var result = service.getById(id).orElse(null);
+        if (result == null){
+            return new ModelAndView("pages/master/error/not-found");
+        }
+        view.addObject("sesi", result);
+        view.addObject("dataTa", tahunAjaranService.get());
+        view.addObject("kurikulum", kurikulumService.get());
+        return view;
     }
 
-    @PostMapping("/update/{id}")
-    public ResponseEntity<Response> update(@RequestBody @Valid SesiRequest request, @PathVariable("id") Integer id){
-        var result = service.update(request, id);
-        return getResponse(result);
+    @PostMapping("/update")
+    public ModelAndView update(@ModelAttribute("sesi") @Valid SesiRequest request, BindingResult result){
+        ModelAndView view = new ModelAndView("pages/master/sesi/edit");
+        List<TahunAjaranResponse> tahunAjaran = this.tahunAjaranService.get();
+        List<KurikulumResponse> kurikulum  = this.kurikulumService.get();
+        if (result.hasErrors()){
+            view.addObject("sesi", request);
+            return view;
+        }
+        view.addObject("dataTa", tahunAjaran);
+        view.addObject("kurikulum", kurikulum);
+        var response = service.update(request, request.getId()).orElse(null);
+        return new ModelAndView("redirect:/master/sesi");
     }
 
     @GetMapping("/delete/{id}")
-    public ModelAndView delete(@PathVariable("id") Integer id){
-        return new ModelAndView("pages/master/sesi/delete");
+    public ModelAndView delete(@PathVariable("id") String id){
+        ModelAndView view = new ModelAndView("pages/master/sesi/delete");
+        var result = service.getById(id).orElse(null);
+        if (result == null){
+            return new ModelAndView("pages/master/error/not-found");
+        }
+        view.addObject("sesi", result);
+        view.addObject("dataTa", tahunAjaranService.get());
+        view.addObject("kurikulum", kurikulumService.get());
+        return view;
     }
 
-    @PostMapping("/remove/{id}")
-    public ResponseEntity<Response> remove(@PathVariable("id") Integer id){
-        var result = service.delete(id);
-        return getResponse(result);
+    @PostMapping("/remove")
+    public ModelAndView remove(@ModelAttribute("sesi") @Valid SesiRequest request, BindingResult result) {
+        ModelAndView view = new ModelAndView("pages/master/sesi/delete");
+        List<TahunAjaranResponse> tahunAjaran = this.tahunAjaranService.get();
+        List<KurikulumResponse> kurikulum = this.kurikulumService.get();
+        if (result.hasErrors()) {
+            view.addObject("sesi", request);
+            return view;
+        }
+        view.addObject("dataTa", tahunAjaran);
+        view.addObject("kurikulum", kurikulum);
+        var response = service.delete(request.getId()).orElse(null);
+        return new ModelAndView("redirect:/master/sesi");
     }
+
 }
--- src/main/java/org/edupro/web/model/request/SesiRequest.java
+++ src/main/java/org/edupro/web/model/request/SesiRequest.java
@@ -1,6 +1,7 @@
 package org.edupro.web.model.request;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,12 +13,20 @@ import lombok.Setter;
 @Getter
 @Setter
 public class SesiRequest {
-    private Integer id;
+    private String id;
 
-    @NotEmpty
-    @Size(max = 20)
-    private String kodeSesi;
+    @NotNull(message = "Tahun Ajaran Id wajib diisi")
+    @Size(min = 36, max = 36, message = "Tahun Ajaran Id minimal 36 dan maksimal 36")
+    private String tahunAjaranId;
 
-    @Size(max = 30)
-    private String namaSesi;
+    @NotNull(message = "Nomor Urut wajid diisi")
+    private Integer urut;
+
+    @NotNull(message = "Kurikulum Id wajid diisi")
+    @Size(min = 36, max = 36, message = "Kurikulum Id minimal 36 dan maksimal 36")
+    private String kurikulumId;
+
+    @NotEmpty(message = "Kode Kurikulum wajid diisi")
+    @Size(min = 2, max = 20, message = "Kode kurikulum minimal 2 dan maksimal 20")
+    private String kodeKurikulum;
 }
