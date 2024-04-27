580aad38bab90d71c76f6173e5aa26aebe6f9bbf
mzaenalmustopa <zaenalebelmetal@gmail.com>
2024-04-27 17:21:02 +0700

    feat : add course service impl

--- src/main/java/org/edupro/web/service/MasterCourseService.java
+++ src/main/java/org/edupro/web/service/MasterCourseService.java
@@ -1,4 +1,15 @@
 package org.edupro.web.service;
 
+import org.edupro.web.model.request.CourseRequest;
+import org.edupro.web.model.response.CourseResponse;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface MasterCourseService {
+    List<CourseResponse> get();
+    Optional<CourseResponse> getById(String id);
+    Optional<CourseResponse> save(CourseRequest courseRequest);
+    Optional<CourseResponse> update(CourseRequest courseRequest, String id);
+    Optional<CourseResponse> delete(String id);
 }
--- src/main/java/org/edupro/web/service/impl/MasterCourseServiceImpl.java
+++ src/main/java/org/edupro/web/service/impl/MasterCourseServiceImpl.java
@@ -1,4 +1,123 @@
 package org.edupro.web.service.impl;
 
-public class MasterCourseServiceImpl {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nimbusds.jose.crypto.opts.OptionUtils;
+import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.util.Strings;
+import org.edupro.web.constant.BackEndUrl;
+import org.edupro.web.model.request.CourseRequest;
+import org.edupro.web.model.response.CourseResponse;
+import org.edupro.web.model.response.Response;
+import org.edupro.web.service.MasterCourseService;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MasterCourseServiceImpl implements MasterCourseService {
+    private final BackEndUrl backEndUrl;
+    private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public List<CourseResponse> get() {
+        try {
+            var url = backEndUrl.courseUrl();
+            ResponseEntity<Response> response = restTemplate.getForEntity(url, Response.class);
+            if (response.getStatusCode() == HttpStatus.OK){
+                return (List<CourseResponse>) response.getBody().getData();
+            }
+        }catch (RestClientException e){
+            return Collections.emptyList();
+        }
+
+        return Collections.emptyList();
+    }
+
+    @Override
+    public Optional<CourseResponse> getById(String id) {
+        try {
+            var url = Strings.concat(backEndUrl.courseUrl(), "/" + id);
+            ResponseEntity<Response> response = restTemplate.getForEntity(url, Response.class);
+            if (response.getStatusCode() == HttpStatus.OK){
+                byte[] json = objectMapper.writeValueAsBytes(Objects.requireNonNull(response.getBody()).getData());
+                CourseResponse result = objectMapper.readValue(json, CourseResponse.class);
+            }
+        }catch (RestClientException e){
+            return Optional.empty();
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<CourseResponse> save(CourseRequest courseRequest) {
+        try {
+            var url = backEndUrl.courseUrl();
+            HttpEntity<CourseRequest> httpEntity = new HttpEntity<>(courseRequest);
+            ResponseEntity<Response> response = restTemplate.postForEntity(url, httpEntity, Response.class);
+            if (response.getStatusCode() == HttpStatus.OK){
+                byte[] json = objectMapper.writeValueAsBytes(Objects.requireNonNull(response.getBody()).getData());
+                CourseResponse result = objectMapper.readValue(json, CourseResponse.class);
+                return Optional.of(result);
+            }
+        }catch (RestClientException e){
+            return Optional.empty();
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<CourseResponse> update(CourseRequest courseRequest, String id) {
+        try {
+            var url = Strings.concat(backEndUrl.courseUrl(), "/" + id);
+            HttpEntity<CourseRequest> httpEntity = new HttpEntity<>(courseRequest);
+            ResponseEntity<Response> response = restTemplate.exchange(url, HttpMethod.PUT, httpEntity, Response.class);
+            if (response.getStatusCode() == HttpStatus.OK){
+                byte[] json = objectMapper.writeValueAsBytes(Objects.requireNonNull(response.getBody()).getData());
+                CourseResponse result = objectMapper.readValue(json, CourseResponse.class);
+                return Optional.of(result);
+            }
+        }catch (RestClientException e){
+            return Optional.empty();
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<CourseResponse> delete(String id) {
+        try {
+            var url = Strings.concat(backEndUrl.courseUrl(), "/" + id);
+            ResponseEntity<Response> response = restTemplate.exchange(url, HttpMethod.DELETE, null, Response.class);
+            if (response.getStatusCode() == HttpStatus.OK){
+                byte[] json = objectMapper.writeValueAsBytes(Objects.requireNonNull(response.getBody()).getData());
+                CourseResponse result = objectMapper.readValue(json, CourseResponse.class);
+            }
+        }catch (RestClientException e){
+            return Optional.empty();
+        }catch (IOException e){
+            throw new RuntimeException();
+        }
+
+        return Optional.empty();
+    }
 }
