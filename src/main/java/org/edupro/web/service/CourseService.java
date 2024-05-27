3f35bea3c4351e89856735ba034712c4c118bf6d
Diktya19 <diktya@gmail.com>
2024-05-27 11:47:12 +0700

    feat: update for index people and add

--- src/main/java/org/edupro/web/service/CourseService.java
+++ src/main/java/org/edupro/web/service/CourseService.java
@@ -2,8 +2,10 @@ package org.edupro.web.service;
 
 import org.edupro.web.exception.EduProWebException;
 import org.edupro.web.model.request.CourseRequest;
+import org.edupro.web.model.response.CoursePeopleResponse;
 import org.edupro.web.model.response.CourseResponse;
 import org.edupro.web.model.response.CourseSectionRes;
+import org.edupro.web.model.response.CourseSiswaResponse;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,6 +14,7 @@ public interface CourseService {
     List<CourseResponse> get() throws EduProWebException;
     List<CourseResponse> getByUser() throws EduProWebException;
     List<CourseSectionRes> getSectionByCourseId(String courseId) throws EduProWebException;
+    List<CoursePeopleResponse> getPeople();
     Optional<CourseResponse> getById(String id) throws EduProWebException;
     Optional<CourseResponse> save(CourseRequest courseRequest);
     Optional<CourseResponse> update(CourseRequest courseRequest, String id);
