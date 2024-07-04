b41373a790e3c3428f3639d27627964e81549f53
mzaenalmustopa <zaenalebelmetal@gmail.com>
2024-07-04 13:49:59 +0700

    feat update istitution

--- src/main/java/org/edupro/web/institution/controller/InstitutionController.java
+++ src/main/java/org/edupro/web/institution/controller/InstitutionController.java
@@ -1,14 +1,20 @@
 package org.edupro.web.institution.controller;
 
+import jakarta.validation.Valid;
 import org.edupro.web.base.controller.BaseController;
 import org.edupro.web.base.model.Response;
 import org.edupro.web.building.model.BuildingRes;
 import org.edupro.web.exception.EduProWebException;
+import org.edupro.web.institution.model.InstitutionReq;
 import org.edupro.web.institution.model.InstitutionRes;
 import org.edupro.web.institution.service.InstitutionService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/master/institution")
@@ -35,6 +42,29 @@ public class InstitutionController extends BaseController<InstitutionRes> {
         ModelAndView view =  new ModelAndView("pages/master/institution/add");
 
         view.addObject("institution", new InstitutionRes());
+        return view;
+    }
+
+    @PostMapping("/save")
+    public ModelAndView save(@ModelAttribute("institution") @Valid InstitutionReq request, BindingResult result) {
+        ModelAndView view = new ModelAndView("pages/master/institution/add");
+        view.addObject("institution", request);
+        if (result.hasErrors()){
+            return view;
+        }
+
+        Optional<InstitutionRes> res;
+        try {
+            res = service.save(request);
+        }catch (EduProWebException e){
+            addError("institution", result,(List<FieldError>) e.getErrors());
+            return view;
+        }
+
+        if (res.isEmpty()){
+            addError("institution", result,Collections.emptyList());
+        }
+
         return view;
     }
 
--- src/main/java/org/edupro/web/institution/model/InstitutionReq.java
+++ src/main/java/org/edupro/web/institution/model/InstitutionReq.java
@@ -1,5 +1,7 @@
 package org.edupro.web.institution.model;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
@@ -12,37 +14,123 @@ import java.time.LocalDate;
 @Builder
 public class InstitutionReq {
     private String id;
-    @Size(max = 200)
+    @NotEmpty(message = "name wajib di isi")
+    @Size(min = 5 , max = 200, message = "minimal 5 dan maksimal 200")
     private String name;
+
+    @NotEmpty(message = "short name wajib di isi")
+    @Size(min = 5 , max = 20, message = "minimal 5 dan maksimal 20")
     private String shortName;
+
+    @NotEmpty(message = "regNumber wajib di isi")
+    @Size(min = 5 , max = 20, message = "minimal 5 dan maksimal 20")
     private String regNumber;
+
+    @NotEmpty(message = "regNumber wajib di isi")
+    @Size(min = 5 , max = 20, message = "minimal 5 dan maksimal 20")
     private String code;
+
+    @NotNull
     private LocalDate expiredDate;
+
+    @NotEmpty(message = "regNumber wajib di isi")
+    @Size(min = 5 , max = 20, message = "minimal 5 dan maksimal 20")
     private String levelCategory;
+
+    @NotEmpty(message = "regNumber wajib di isi")
+    @Size(min = 5 , max = 20, message = "minimal 5 dan maksimal 20")
     private String headmaster;
+
+    @NotEmpty(message = "regNumber wajib di isi")
+    @Size(min = 5 , max = 20, message = "minimal 5 dan maksimal 20")
     private String uniqueNumber;
+
+    @NotEmpty(message = "regNumber wajib di isi")
+    @Size(min = 5 , max = 20, message = "minimal 5 dan maksimal 20")
     private String adminName;
+
+    @NotNull
     private int maxExamUser;
+    @NotNull(message = "must not be null")
     private int maxLmsUser;
+    @NotNull(message = "must not be null")
     private int diffServerTime;
+    @NotNull(message = "must not be null")
     private int effectiveDays;
+
+    @NotEmpty(message = "startedDay wajib di isi")
+    @Size(min = 5 , max = 20, message = "minimal 5 dan maksimal 20")
     private String startedDay;
+
+    @NotEmpty(message = "endDay wajib di isi")
+    @Size(min = 5 , max = 20, message = "minimal 5 dan maksimal 20")
     private String endDay;
+
+    @NotEmpty(message = "endEarly wajib di isi")
+    @Size(min = 5 , max = 20, message = "minimal 5 dan maksimal 20")
     private String endEarly;
+
+    @NotEmpty(message = "endOfDay wajib di isi")
+    @Size(min = 5 , max = 20, message = "minimal 5 dan maksimal 20")
     private String endOfDay;
+
+    @NotEmpty(message = "provinceId wajib di isi")
+    @Size(min = 5 , max = 20, message = "minimal 5 dan maksimal 20")
     private String provinceId;
+
+    @NotEmpty(message = "cityId wajib di isi")
+    @Size(min = 5 , max = 20, message = "minimal 5 dan maksimal 20")
     private String cityId;
+
+    @NotEmpty(message = "districtId wajib di isi")
+    @Size(min = 5 , max = 20, message = "minimal 5 dan maksimal 20")
     private String districtId;
+
+    @NotEmpty(message = "subDistrictId wajib di isi")
+    @Size(min = 5 , max = 20, message = "minimal 5 dan maksimal 20")
     private String subDistrictId;
+
+    @NotEmpty(message = "address wajib di isi")
+    @Size(min = 5 , max = 20, message = "minimal 5 dan maksimal 20")
     private String address;
+
+    @NotEmpty(message = "postalCode wajib di isi")
+    @Size(min = 5 , max = 20, message = "minimal 5 dan maksimal 20")
     private String postalCode;
+
+    @NotEmpty(message = "phoneNumber wajib di isi")
+    @Size(min = 5 , max = 20, message = "minimal 5 dan maksimal 20")
     private String phoneNumber;
+
+    @NotEmpty(message = "faxNumber wajib di isi")
+    @Size(min = 5 , max = 20, message = "minimal 5 dan maksimal 20")
     private String faxNumber;
+
+    @NotEmpty(message = "website wajib di isi")
+    @Size(min = 5 , max = 20, message = "minimal 5 dan maksimal 20")
     private String website;
+
+    @NotEmpty(message = "email wajib di isi")
+    @Size(min = 5 , max = 20, message = "minimal 5 dan maksimal 20")
     private String email;
+
+    @NotEmpty(message = "letterHead wajib di isi")
+    @Size(min = 5 , max = 20, message = "minimal 5 dan maksimal 20")
     private String letterHead;
+
+    @NotEmpty(message = "headOfSignature wajib di isi")
+    @Size(min = 5 , max = 20, message = "minimal 5 dan maksimal 20")
     private String headOfSignature;
+
+    @NotEmpty(message = "serviceLogo wajib di isi")
+    @Size(min = 5 , max = 20, message = "minimal 5 dan maksimal 20")
     private String serviceLogo;
+
+    @NotEmpty(message = "institutionLogo wajib di isi")
+    @Size(min = 5 , max = 20, message = "minimal 5 dan maksimal 20")
     private String institutionLogo;
+
+    @NotEmpty(message = "stamp wajib di isi")
+    @Size(min = 5 , max = 20, message = "minimal 5 dan maksimal 20")
     private String stamp;
 }
